4328cb068e699649f1137d5b4b75a823c74d7ee2
Harry-Daniel <hayiborkomla@gmail.com>
2026-02-05 14:21:25 +0000

    Worked on alignment

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [proposalStage, setProposalStage] = useState(1);
   const [size, setSize] = useState({
     width: 0,
     height: 0,
@@ -32,55 +33,60 @@ export default function Home() {
 
   const handleOnClickYes = () => {
     console.log("yes");
+    setProposalStage(2);
   };
 
   return (
     <section className="bg-red-300 h-screen w-screen flex justify-center items-center">
-      <div className="bg-white w-[60%] h-[60%] rounded-2xl shadow-2xl flex flex-col justify-center items-center gap-12 p-8">
-        <div className="p-4 flex flex-col gap-4">
-          <p className="texl-lg lg:text-2xl text-center">
-            <span className="text-red-300 font-bold">
-              Karen Mmabilla Adazabra
-            </span>{" "}
-            will you be my Valentiwwwne???
-          </p>
-          <div className="flex flex-row justify-center gap-8 lg:gap-32">
-            <button
-              onClick={handleOnClickYes}
-              className="bg-red-300 text-white font-bold px-4 py-1 rounded-xl active:scale-95 hover:scale-105"
-            >
-              Yes
-            </button>
-            <div className="relative">
-              <button
-                onClick={handleOnClickYes}
-                className="bg-red-300 text-white font-bold px-4 py-1 rounded-xl active:scale-95 hover:scale-105 absolute"
-              >
-                Yes
-              </button>
-              {visible && (
+      <div className="bg-white w-[60%] h-[60%] rounded-2xl shadow-2xl flex justify-center items-center ">
+        {proposalStage === 1 && (
+          <div className="flex flex-col justify-center items-center gap-12 p-8">
+            <div className=" flex flex-col gap-4 justify-center items-center">
+              <p className="texl-lg lg:text-2xl text-center">
+                <span className="text-red-300 font-bold">
+                  Karen Mmabilla Adazabra
+                </span>{" "}
+                will you be my Valentiwwwne???
+              </p>
+              <div className="flex flex-row  ">
                 <button
-                  onMouseEnter={() => setVisible(false)}
-                  onClick={() => setVisible(false)}
-                  className="bg-red-300 text-white font-bold px-4 py-1 rounded-xl text-center  absolute"
+                  onClick={handleOnClickYes}
+                  className="bg-red-300 text-white font-bold px-4 py-1 w-16 h-8 rounded-xl active:scale-95 hover:scale-105"
                 >
-                  No
+                  Yes
                 </button>
+                <div className="relative w-16 h-8">
+                  <button
+                    onClick={handleOnClickYes}
+                    className="bg-red-300 text-white font-bold px-4 py-1 w-16 h-8 rounded-xl active:scale-95 hover:scale-105 absolute"
+                  >
+                    Yes
+                  </button>
+                  {visible && (
+                    <button
+                      onMouseEnter={() => setVisible(false)}
+                      onClick={() => setVisible(false)}
+                      className="bg-red-300 text-white font-bold px-4 py-1 w-16 h-8 rounded-xl text-center  absolute"
+                    >
+                      No
+                    </button>
+                  )}
+                </div>
+              </div>
+              {!visible && (
+                <p className="text-center text-pink-800">
+                  It's vals baby not April fools, let's stop the jokes 🩷
+                </p>
               )}
             </div>
+            <Image
+              src={"/assets/images/asking.jpg"}
+              alt="Cutie asking you to be his valentine"
+              width={size.width}
+              height={size.height}
+            />
           </div>
-          {!visible && (
-            <p className="text-center text-pink-800">
-              It's vals baby not April fools, let's stop the jokes 🩷
-            </p>
-          )}
-        </div>
-        <Image
-          src={"/assets/images/asking.jpg"}
-          alt="Cutie asking you to be his valentine"
-          width={size.width}
-          height={size.height}
-        />
+        )}
       </div>
     </section>
   );
